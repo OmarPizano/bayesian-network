9c98fdd20f88ce8c564b116765bf5cfdc63302b9
OmarPizano <oj.pizanoorozco@ugto.mx>
2022-04-22 11:25:02 -0500

    Add more nodes to the network.

--- bn.py
+++ bn.py
@@ -17,7 +17,7 @@ def joint_prob(df:pd.DataFrame, values:dict, bn:dict) -> float:
         for dep in bn[node]:
             deps[dep] = values[dep]
         probs.append(label_prob(df, node, values[node], deps)) 
-    #print(probs)
+    print(probs)
     return np.prod(probs) # p(x1,x2,..xn) = p(x1|deps) * p(x2|deps) * ... * p(xn|deps)
 
 def get_class_probs(df:pd.DataFrame, class_ft:str, sample:dict, bn:dict, labels:dict) -> dict:
@@ -42,34 +42,41 @@ def categorize_binary(col:pd.Series, labels:list, bins:list) -> pd.Series:
 
 
 bn = {
-        'sexo': ['peso', 'estatura', 'pie'],
-        'peso': ['estatura'],
+        'sexo': ['peso', 'pie'],
         'estatura': [],
-        'pie': []
+#        'edad': [],
+        'pie': ['estatura'],
+#        'brazo': ['edad'],
+        'espalda': ['sexo'],
+        'craneo' : ['sexo'],
+        'peso': ['estatura'],
         }
 
 labels = {
         'sexo': ['hombre', 'mujer'],
         'estatura': ['baja', 'alta'],
-#        'edad': ['joven', 'viejo']
+#        'edad': ['joven', 'viejo'],
         'pie': ['chico', 'grande'],
 #        'brazo': ['corto', 'largo'],
-#        'espalda': ['delgada', 'robusta'],
-#        'craneo': ['chico', 'grande'],
-        'peso': ['ligero', 'pesado']
+        'espalda': ['delgada', 'robusta'],
+        'craneo': ['chico', 'grande'],
+        'peso': ['ligero', 'pesado'],
         }
 
 df = pd.read_csv('dataset.csv')
 
-df = df.drop(['edad', 'brazo', 'espalda', 'craneo'], axis=1)
+df = df.drop(['edad', 'brazo'], axis=1)
 
-df['estatura'] = categorize_binary(df['estatura'], labels['estatura'], [120, 170, 200])
-# df['edad'] = categorize_binary(df['edad'], labels['edad'], [1, 35, 100])
+df['estatura'] = categorize_binary(df['estatura'], labels['estatura'], [120, 160, 200])
+#df['edad'] = categorize_binary(df['edad'], labels['edad'], [1, 35, 100])
 df['pie'] = categorize_binary(df['pie'], labels['pie'], [15, 24, 30])
-# df['brazo'] = categorize_binary(df['brazo'], labels['brazo'], [45, 60, 75])
-# df['espalda'] = categorize_binary(df['espalda'], labels['espalda'], [30, 45, 60])
-# df['craneo'] = categorize_binary(df['craneo'], labels['craneo'], [45, 54, 60])
+#df['brazo'] = categorize_binary(df['brazo'], labels['brazo'], [45, 60, 75])
+df['espalda'] = categorize_binary(df['espalda'], labels['espalda'], [30, 45, 60])
+df['craneo'] = categorize_binary(df['craneo'], labels['craneo'], [45, 54, 60])
 df['peso'] = categorize_binary(df['peso'], labels['peso'], [25, 70, 110])
 
+print(df)
+
 # Test
-print(get_class_probs(df, 'sexo', {'estatura':'alta', 'peso':'pesado', 'pie':'grande'}, bn, labels))
+sample = {'estatura':'alta', 'pie':'grande', 'espalda':'robusta', 'craneo':'grande', 'peso':'pesado'}
+print(get_class_probs(df, 'sexo', sample, bn, labels))
